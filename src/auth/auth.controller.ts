04455d9b46e74d162ff9a67e58cd5a9823df4ce3
wbhaao <a31620504@gmail.com>
2023-09-11 06:05:05 +0900

    커스텀 데코레이터

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-// import { GetUser } from './get-user.decorator';
+import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
 
 @Controller('auth')
@@ -30,9 +30,9 @@ export class AuthController {
     return this.authService.signIn(authCredentialsDto);
   }
 
-  //   @Post('/test')
-  //   @UseGuards(AuthGuard())
-  //   test(@GetUser() user: User) {
-  //     console.log('user', user);
-  //   }
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@GetUser() user: User) {
+    console.log('user', user);
+  }
 }
